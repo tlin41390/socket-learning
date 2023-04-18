0d1fdf22452639c5386aa8e9b6b316c76298e6ac
Your Name <you@example.com>
2023-04-17 22:58:52 -0700

    created simple client server connection.

--- socket_learning/server/index.js
+++ socket_learning/server/index.js
@@ -13,6 +13,14 @@ const io = new Server(server, {
     },
 });
 
+io.on("connection",(socket)=>{
+    console.log(`User connected: ${socket.id}`);
+
+    socket.on("send_message",(data)=>{
+        socket.broadcast.emit("receive_message",data);
+    })
+})
+
 server.listen(3001, () => {
     console.log("listening on 3001");
 });
